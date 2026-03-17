6306d72798b62e710560c42399f5a4bb79e059af
HiorCraft <liam.fanenbruck@gmail.com>
2026-03-17 20:57:20 +0100

    Feat: add EntitySpawn Listener

---
+++ src/main/kotlin/de/hiorcraft/nex/nexLobby/listener/EntitySpawnListener.kt
@@ -0,0 +1,13 @@
+package de.hiorcraft.nex.nexLobby.listener
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntitySpawnEvent
+
+object EntitySpawnListener : Listener {
+
+    @EventHandler
+    fun onSpawn(event: EntitySpawnEvent) {
+        event.isCancelled = true
+    }
+}
