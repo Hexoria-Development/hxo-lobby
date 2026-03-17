c3de312db1d77ef8db141d1d4fd95fdf41938309
HiorCraft <liam.fanenbruck@gmail.com>
2026-03-17 20:57:52 +0100

    Feat: add PlayerConnection Listener

---
+++ src/main/kotlin/de/hiorcraft/nex/nexLobby/listener/PlayerConnectionListener.kt
@@ -0,0 +1,26 @@
+package de.hiorcraft.nex.nexLobby.listener
+
+import de.hiorcraft.nex.nexLobby.inventory.InventoryItem
+import org.bukkit.GameMode
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+
+object PlayerConnectionListener: Listener {
+
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        event.player.gameMode = GameMode.ADVENTURE
+        event.player.inventory.heldItemSlot = 4
+
+        for (i in 0..8) {
+            event.player.inventory.clear(i)
+        }
+
+        InventoryItem.items.filter { item ->
+            item.permission?.let { event.player.hasPermission(it) } ?: true
+        }.forEach {
+            event.player.inventory.setItem(it.slot, it.getItemForPlayer(event.player))
+        }
+    }
+}
