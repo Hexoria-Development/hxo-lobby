ad81e2518001e55e591eebcea42ba603a5ff8760
HiorCraft <liam.fanenbruck@gmail.com>
2026-04-20 19:57:06 +0200

    feat: add Reload Command

--- src/main/kotlin/de/hiorcraft/nex/Lobby/PaperMain.kt
+++ src/main/kotlin/de/hiorcraft/nex/Lobby/PaperMain.kt
@@ -1,15 +1,30 @@
 package de.hiorcraft.nex.Lobby
 
+import de.hiorcraft.nex.Lobby.command.lobbyCommand
+import de.hiorcraft.nex.Lobby.command.spawnCommand
+import de.hiorcraft.nex.Lobby.config.LobbyConfigHolder
 import de.hiorcraft.nex.Lobby.hook.npc.SurfNpcHook
 import de.hiorcraft.nex.Lobby.listener.DamageListener
+import de.hiorcraft.nex.Lobby.listener.DoubleJump
 import de.hiorcraft.nex.Lobby.listener.EntitySpawnListener
 import de.hiorcraft.nex.Lobby.listener.FoodListener
+import de.hiorcraft.nex.Lobby.listener.InventoryInteractListener
+import de.hiorcraft.nex.Lobby.listener.ItemInteractListener
+import de.hiorcraft.nex.Lobby.listener.PlayerConnectionListener
+import de.hiorcraft.nex.Lobby.listener.PlayerMoveListener
+import de.hiorcraft.nex.Lobby.listener.PushbackListener
+import de.hiorcraft.nex.Lobby.listener.ShiftBoostListener
+import de.hiorcraft.nex.Lobby.listener.SpawnLocationListener
 import de.hiorcraft.nex.Lobby.listener.WorldProtectionListener
+import de.hiorcraft.nex.Lobby.listener.XpBarListener
+import de.hiorcraft.nex.Lobby.manager.PushbackManager
 import org.bukkit.Bukkit
+import org.bukkit.GameRule
 import org.bukkit.plugin.java.JavaPlugin
 
 val plugin get() = JavaPlugin.getPlugin(PaperMain::class.java)
 val surfNpcHook get() = Bukkit.getPluginManager().isPluginEnabled("surf-npc-paper")
+lateinit var lobbyConfigHolder: LobbyConfigHolder
 
 class PaperMain : JavaPlugin() {
 
@@ -17,10 +32,13 @@ class PaperMain : JavaPlugin() {
 
         logger.info("is starting.....")
 
+        lobbyConfigHolder = LobbyConfigHolder()
 
         if (surfNpcHook) {
             SurfNpcHook.initialize()
         }
+
+        PushbackManager.startTask()
 
         val manager = server.pluginManager
 
@@ -28,13 +46,29 @@ class PaperMain : JavaPlugin() {
         manager.registerEvents(FoodListener(), this)
         manager.registerEvents(WorldProtectionListener(), this)
         manager.registerEvents(EntitySpawnListener, this)
+        manager.registerEvents(SpawnLocationListener, this)
+        manager.registerEvents(ItemInteractListener, this)
+        manager.registerEvents(InventoryInteractListener, this)
+        manager.registerEvents(PushbackListener(), this)
+        manager.registerEvents(PlayerConnectionListener, this)
+        manager.registerEvents(PlayerMoveListener, this)
+        manager.registerEvents(DoubleJump(), this)
+        manager.registerEvents(XpBarListener, this)
+        manager.registerEvents(ShiftBoostListener, this)
 
         logger.info("Listener registered")
 
-
+        spawnCommand()
+        lobbyCommand()
         logger.info("Commands registered")
     }
 
     override fun onDisable() {
+        logger.info("is disabled....")
+
+        logger.info("lobby config is disabled")
+
+        logger.info("Bye :)")
     }
 }
+
--- src/main/kotlin/de/hiorcraft/nex/Lobby/utils/PermissionRegistry.kt
+++ src/main/kotlin/de/hiorcraft/nex/Lobby/utils/PermissionRegistry.kt
@@ -8,7 +8,11 @@ object PermissionRegistry : PermissionRegistry() {
     private const val COMMAND_PREFIX = "$PREFIX.command"
 
     val COMMAND_SPAWN = create("$COMMAND_PREFIX.spawn")
-    val COMMAND_LOBBY = create("$COMMAND_PREFIX.lobby")
     val COMMAND_LOBBY_RELOAD = create("$COMMAND_PREFIX.lobby.reload")
+
+    val PUSHBACK_ATTACK = create("$PREFIX.pushback.attack")
+    val PUSHBACK_ITEM = create("$PREFIX.pushback.item")
+
+    val ELYTRA_BOOST = create("$PREFIX.elytraboost")
 
 }
